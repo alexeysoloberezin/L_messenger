8fe641c15cdbc4763df365f308e64706eb485ecf
Леонид Иванов <ivanov.l@robotkarl.com>
2023-12-02 16:44:49 +0800

    try build 1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -60,6 +60,4 @@ export const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
 }
 
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
+export default NextAuth(authOptions);
